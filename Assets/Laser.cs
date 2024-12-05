345c26493b2034082ff158e5b823a310e4ba0074
MochiBingo <mochibingoshep@gmail.com>
2024-12-05 15:22:10 -0400

    line in game view

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public int maxBounce;
+    public int maxBounce = 5;
     public Transform laserOrigin;
     public GameObject laserEnd;
     public Material power;
     public Material off;
+    public LineRenderer line;
     private void OnDrawGizmos()
     {
         Vector3 currentPosition = laserOrigin.position;
         Vector3 currentDirection = laserOrigin.transform.up;
+        line.SetPosition(0, currentPosition);
 
         for (int i = 0; i < maxBounce; i++)
         {
             RaycastHit hit;
             if (Physics.Raycast(currentPosition, currentDirection, out hit, 200.0f) && hit.collider.CompareTag("Mirror"))
             {
-                Gizmos.color = Color.red;
-                Gizmos.DrawLine(currentPosition, hit.point);
+                
+                line.SetPosition(i, hit.point);
                 currentDirection = Vector3.Reflect(currentDirection, hit.normal);
                 currentPosition = hit.point;
+                if (Physics.Raycast(currentPosition, currentDirection, out hit, 200.0f) && hit.collider.CompareTag("LaserAccepter"))
+                {
+                    line.SetPosition(i+1, hit.point);
+                }
             }
             else
             {
@@ -31,7 +37,7 @@ public class Laser : MonoBehaviour
             }
             if (Physics.Raycast(currentPosition, currentDirection, out hit, 200.0f) && hit.collider.CompareTag("LaserAccepter"))
             {
-                laserEnd.GetComponent<Renderer>().material = power; 
+                laserEnd.GetComponent<Renderer>().material = power;
             }
             else
             {
